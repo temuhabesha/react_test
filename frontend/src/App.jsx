d913423c7096c1ae456abc705f37c651ed812aa8
ethiotom <temesgenwondim77@gmail.com>
2025-07-16 02:27:47 +0300

    this is the first commit for this code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="reactTest">
+        <h1>this is the inroduction of react js class</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
