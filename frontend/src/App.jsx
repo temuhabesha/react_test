374006456c5dc01fb2df380c452cfeb6a5b75ee3
ethiotom <temesgenwondim77@gmail.com>
2025-07-16 02:31:11 +0300

    this is the second commit for this code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <div className="reactTest">
         <h1>this is the inroduction of react js class</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fugiat ut delectus harum tempora dolor, excepturi nostrum aut maiores, enim culpa amet, officia itaque accusantium veritatis. Numquam voluptatibus similique harum?</p>
       </div>
     </>
   )
